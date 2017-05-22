0b3cadf33dced1552c7eaa78fd0a13ae51c20fc9
彭志浩 <ChihoPang@163.com>
2017-05-22 15:54:19 +0800

    引入下拉刷新

--- app/src/main/java/com/chihopang/readhub/base/BaseListFragment.java
+++ app/src/main/java/com/chihopang/readhub/base/BaseListFragment.java
@@ -3,6 +3,7 @@ package com.chihopang.readhub.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,7 +14,8 @@ import com.chihopang.readhub.feature.main.MainActivity;
 import java.util.List;
 
 public abstract class BaseListFragment<T> extends Fragment {
-  private RecyclerView mRecyclerVIew;
+  private RecyclerView mRecyclerView;
+  private SwipeRefreshLayout mSwipeRefreshLayout;
   private BaseAdapter<T> mAdapter = new BaseAdapter<T>() {
     @Override public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
       return provideViewHolder(parent, viewType);
@@ -25,12 +27,22 @@ public abstract class BaseListFragment<T> extends Fragment {
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_base_list, container, false);
-    mRecyclerVIew = (RecyclerView) view.findViewById(R.id.recycler_view);
-    mRecyclerVIew.setAdapter(mAdapter);
-    mRecyclerVIew.setLayoutManager(mManager);
+    mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+    mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
+    initContent();
     ((MainActivity) getActivity()).mBox.showLoadingLayout();
     requestData();
     return view;
+  }
+
+  private void initContent() {
+    mRecyclerView.setAdapter(mAdapter);
+    mRecyclerView.setLayoutManager(mManager);
+    mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override public void onRefresh() {
+        requestData();
+      }
+    });
   }
 
   protected abstract void requestData();
@@ -38,6 +50,8 @@ public abstract class BaseListFragment<T> extends Fragment {
   public void onSuccess(final List<T> itemList) {
     getActivity().runOnUiThread(new Runnable() {
       @Override public void run() {
+        if (mSwipeRefreshLayout.isRefreshing()) mSwipeRefreshLayout.setRefreshing(false);
+        mAdapter.clear();
         mAdapter.addItems(itemList);
         ((MainActivity) getActivity()).mBox.hideAll();
       }
