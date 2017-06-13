95387cc193a34926059e7a12b1101fbf997d1316
彭志浩 <chihopang@163.com>
2017-06-13 20:29:54 +0800

    添加 加载webview 功能

--- app/src/main/java/com/chihopang/readhub/feature/news/ArticlePageActivity.java
+++ app/src/main/java/com/chihopang/readhub/feature/news/ArticlePageActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import butterknife.BindView;
 import com.chihopang.readhub.R;
 import com.chihopang.readhub.app.Navigator;
@@ -16,8 +20,8 @@ import org.parceler.Parcels;
 
 public class ArticlePageActivity extends BaseActivity {
   @BindView(R.id.toolbar) Toolbar mToolbar;
+  @BindView(R.id.progress_bar_loading_web) ProgressBar mProgressBar;
   @BindView(R.id.web_view_article_page) WebView mWebView;
-
   private Topic mTopic;
 
   public static void start(Activity startingActivity, Topic topic) {
@@ -34,7 +38,6 @@ public class ArticlePageActivity extends BaseActivity {
   }
 
   private void initContent() {
-    mToolbar.setTitle(mTopic.getTitle());
     setSupportActionBar(mToolbar);
     mToolbar.setNavigationIcon(R.drawable.ic_back);
     mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -42,6 +45,40 @@ public class ArticlePageActivity extends BaseActivity {
         finish();
       }
     });
+    mToolbar.setTitle(mTopic.getTitle());
+    mWebView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        //防止跳转到浏览器
+        view.loadUrl(url);
+        return super.shouldOverrideUrlLoading(view, url);
+      }
+    });
+    mWebView.setWebChromeClient(new WebChromeClient() {
+      @Override public void onProgressChanged(WebView view, int newProgress) {
+        super.onProgressChanged(view, newProgress);
+        if (newProgress == 100) {
+          mProgressBar.setVisibility(View.GONE);
+          return;
+        }
+        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBar.setProgress(newProgress);
+      }
+
+      @Override public void onReceivedTitle(WebView view, String title) {
+        super.onReceivedTitle(view, title);
+        mToolbar.setTitle(title);
+      }
+    });
+    WebSettings mWebSetting = mWebView.getSettings();//TODO 设定支持js
     mWebView.loadUrl(mTopic.getUrl());
+  }
+
+  @Override public void onBackPressed() {
+    if (mWebView.canGoBack()) {
+      mWebView.goBack();
+      return;
+    }
+    super.onBackPressed();
   }
 }
