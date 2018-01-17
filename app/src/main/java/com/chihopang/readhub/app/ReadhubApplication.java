85d30a348172a9a44aa579f916b822c913c65252
彭志浩 <ChihoPang@163.com>
2018-01-17 21:51:21 +0800

    接入 bugly 应用升级 sdk

--- app/src/main/java/com/chihopang/readhub/app/ReadhubApplication.java
+++ app/src/main/java/com/chihopang/readhub/app/ReadhubApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.tencent.bugly.crashreport.CrashReport;
+import com.tencent.bugly.Bugly;
 import me.yokeyword.fragmentation.BuildConfig;
 import me.yokeyword.fragmentation.Fragmentation;
 import me.yokeyword.fragmentation.helper.ExceptionHandler;
@@ -15,7 +15,7 @@ public class ReadhubApplication extends Application {
   @Override public void onCreate() {
     super.onCreate();
     Fresco.initialize(this);
-    CrashReport.initCrashReport(getApplicationContext(), Constant.BUGGLY_APP_ID, false);
+    Bugly.init(getApplicationContext(), Constant.BUGGLY_APP_ID, false);
     Fragmentation.builder()
         .stackViewMode(Fragmentation.BUBBLE)
         .debug(BuildConfig.DEBUG)
